b01bc8cd05bd5b80c16626672664f7e25e873579
scarf <greenscarf005@gmail.com>
2022-01-03 00:34:09 +0900

    feat: :sparkles: xml and url

--- _config.ts
+++ _config.ts
@@ -1,25 +1,19 @@
 import lume from 'lume'
 import code_highlight from 'lume/plugins/code_highlight.ts'
+import date from 'lume/plugins/date.ts'
 import postcss from 'lume/plugins/postcss.ts'
 import pug from 'lume/plugins/pug.ts'
 import relative_urls from 'lume/plugins/relative_urls.ts'
-import terser from "lume/plugins/terser.ts"
-// import base_path from "lume/plugins/base_path.ts"
-// import bundler from "lume/plugins/bundler.ts"
-// import modify_urls from "lume/plugins/modify_urls.ts"
-// import resolve_urls from "lume/plugins/resolve_urls.ts"
-// import slugify_urls from "lume/plugins/slugify_urls.ts"
-// import svgo from "lume/plugins/svgo.ts"
+import terser from 'lume/plugins/terser.ts'
 
 const site = lume({
-  watcher: {
-    ignore: ['.git/*'],
-  },
+  location: new URL('www.maribel.dev'),
 })
 
 site
   .ignore('README.md')
   .use(pug())
+  .use(date())
   .use(code_highlight())
   .use(postcss())
   .use(relative_urls())
