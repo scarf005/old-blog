1f52eb1a8c63b91fc844d32c8720d5b173bab6d5
scarf <greenscarf005@gmail.com>
2022-01-09 01:15:44 +0900

    fix: :heavy_plus_sign: date plugin was needed in sitemap.xml

--- _config.ts
+++ _config.ts
@@ -1,6 +1,6 @@
 import lume from 'lume'
 import code_highlight from 'lume/plugins/code_highlight.ts'
-// import date from 'lume/plugins/date.ts'
+import date from 'lume/plugins/date.ts'
 import postcss from 'lume/plugins/postcss.ts'
 import pug from 'lume/plugins/pug.ts'
 import relative_urls from 'lume/plugins/relative_urls.ts'
@@ -14,6 +14,7 @@ site
   .copy('CNAME')
   .ignore('README.md')
   .use(pug())
+  .use(date())
   .use(code_highlight())
   .use(postcss())
   .use(relative_urls())
